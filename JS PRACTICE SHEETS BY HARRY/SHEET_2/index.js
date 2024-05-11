eb7649b4ffb2ad4baa4ef90df39642dbf4918def
HARENDRA SINGH <79547333+HARENDRA-93@users.noreply.github.com>
2024-05-11 22:02:36 +0530

    Add files via upload

---
+++ JS PRACTICE SHEETS BY HARRY/SHEET_2/index.js
@@ -0,0 +1,63 @@
+ // Q1. Use logic operations to find whether the age of a person lies between 10 and 20 ?
+
+// let a = prompt('what is your age');
+// if(a>10 && a<20)
+//   console.log("true");
+// else
+//   console.log("false");
+
+// Q2. Demonstrate the use of switch case statements in javascript.
+let b = prompt("what day is today");
+b = Number.parseInt(b);
+switch (b) {
+  case 0: console.log("sunday");
+    break;
+  case 1: console.log("monday");
+    break;
+  case 2: console.log("tuesday");
+    break;
+  case 3: console.log("wednesday");
+    break;
+  case 4: console.log("thuraday");
+    break;
+  case 5: console.log("friday");
+    break;
+  case 6: console.log("saturday");
+    break;
+  default: console.log("invalid input");
+}
+// // Q3. Write a javascript program to find whether a number is divisible by 2 and 3.
+
+//  let c = prompt("enter a number");
+// if(c%2==0 && c%3==0)
+// {
+//   console.log("it is divisible by 2 and 3");
+// }
+
+//   // Q4. Write a javascript program to find whether a number is divisible by either 2 or 3.
+
+//   let d = prompt("enter a number");
+// if(d%2==0)
+// {
+//   console.log("it is divisible by 2 ");
+// }
+
+// else if (d%3 == 0)
+// {
+//   console.log("it is divisible by 3");
+// }
+// else 
+// {
+//   console.log("it is a prime number")
+// }
+// //Q5. Print "you can drive" or "you can not drive" based on age being greater  than 18 using ternary operator.
+
+// let age = prompt("enter your age");
+// age = Number.parseInt(age);
+// console.log(age>18? "you can drive" : "you can not drive");
+
+
+
+
+
+
